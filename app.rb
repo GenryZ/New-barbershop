0270189732836df2975719025d1a647d9e5185b4
GenryZ <77881488+GenryZ@users.noreply.github.com>
2021-02-18 15:52:27 +0300

    some fix

--- app.rb
+++ app.rb
@@ -2,8 +2,9 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+require 'rake'
 
-set :database, {adapter:"sqlite3", database: "barbershop.db"}
+set :database, {adapter:"sqlite3", database:"barbershop.db"}
 
 class Client < ActiveRecord::Base
 	validates :name, presence: true, length: { minimum: 3 }
